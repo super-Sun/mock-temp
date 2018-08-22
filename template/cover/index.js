258ad76f3cdc01c1991e872af93ba54815214fba
孙璐炜 <luwei.sun@trustway.cn>
2018-08-22 18:11:50 +0800

    update files

--- template/cover/index.js
+++ template/cover/index.js
@@ -1,15 +1,16 @@
-import instance from './instance';
-import { convertRESTAPI } from '{{$$.relative("util")}}';
+import instance from './instance'
+import { convertRESTAPI } from '{{$$.relative("util")}}'
 
 <% _.forEach(data.mocks, function(mock){ %>/** {{mock.description}} */
-function {{$$.convertMethod(mock)}}(opts) {
+function {{$$.convertMethod(mock)}}(params) {
+  <% if(mock.method == 'post') {%>let opts = { data: params }<%} else {%>let opts = { params: params }<% } %>
   return instance({
     method: '{{mock.method}}',
-    url: <% if($$.isREST(mock.url)) {%>convertRESTAPI('{{mock.url}}', opts)<%} else {%> '{{mock.url}}'<% } %>,
+    url:<% if($$.isREST(mock.url)) {%>convertRESTAPI('{{mock.url}}', opts)<%} else {%> '{{mock.url}}'<% } %>,
     opts: opts
-  });
+  })
 }
 
 <% }) %>export {<% _.forEach(data.mocks, function(mock, i){ %>
   {{$$.convertMethod(mock)}}<% if(data.mocks.length - 1 !== i) { %>,<% } %><% }) %>
-};
+}
